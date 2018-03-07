3f5436193adcfb2d86ac6cab676fff86f15cb11f
Alex Waggoner <waggoner.alex@gmail.com>
2018-03-07 12:17:24 -0800

    set up repo

---
+++ truffle.js
@@ -0,0 +1,17 @@
+module.exports = {
+  networks: {
+		development: {
+			host: "localhost",
+			port: 8545,
+			network_id: "*", // Match any network id
+			gas: 7900000
+		}
+	}
+	// },
+	// solc: {
+	// 	optimizer: {
+	// 		enabled: true,
+	// 		runs: 200
+	// 	}
+	// }
+};
